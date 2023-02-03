0df2d61ba938122de8f55ec9fc1645b6ba1a4c85
HoThinh468 <109681288+HoThinh468@users.noreply.github.com>
2023-02-03 23:18:53 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Job finder ML model
+This is a machine learning model that is trained to use for job finder mobile application. The main feature of the app is to predict whether a job posting is fraudulent or not.
+We use python programming language to train this model and use Flask to deploy it as an API.
+The dataset used in this model is from Kaggle: https://www.kaggle.com/datasets/shivamb/real-or-fake-fake-jobposting-prediction
+
+# Install job finder mobile app
+Click on this link and clone this repository: https://github.com/leductrung2310/JobFinder (make sure you have git installed on your local machine)
+
+After that open android studio and open the folder where you save your repository. 
+
+Click on the run button and now the app is ready but the predict feature is not yet, go to the next section
+
+# Run the model API
+To run the model please follow these steps:
+- Step 1: Clone this repository using git
+- Step 2: Open the folder with any code editor and make sure you have python and other required libraries installed (through command: pip install -r requirements.txt)
+- Step 3: Open terminal and type "python app.py" and now the job finder is just ready to be used
---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+from joblib import load
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+app = Flask(__name__)
+
+# prediction function
+def ValuePredictor(query):
+    loaded_model = load(open("./model/model.pkl", "rb"))
+    result = loaded_model.predict(query)
+    return result[0]
+
+@app.route("/result", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        content = request.get_json(force = True)
+        df = pd.json_normalize(content)
+        le = LabelEncoder()
+        df['title'] = le.fit_transform(df['title'])
+        df['location'] = le.fit_transform(df['location'])
+        df['company_profile'] = le.fit_transform(df['company_profile'])
+        df['requirements'] = le.fit_transform(df['requirements'])
+        df['employment_type'] = le.fit_transform(df['employment_type'])
+        df['required_experience'] = le.fit_transform(df['required_experience'])
+        df['required_education'] = le.fit_transform(df['required_education'])
+        df['industry'] = le.fit_transform(df['industry'])
+        df['function'] = le.fit_transform(df['function'])
+        df['description'] = le.fit_transform(df['description'])
+        is_fraud = ValuePredictor(df)
+        return jsonify(predict = str(is_fraud))
+    else:
+        return 'None'
+
+@app.route("/", methods=['POST'])
+def welcome():
+        return "Welcome to job finder"
+
+if __name__ == '__main__':
+    app.run(debug=True)
